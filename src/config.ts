f315acfb5fe778b9ae85103cc528334fab1a9546
biud436 <biud436@gmail.com>
2021-12-20 17:43:11 +0900

    WATCH_DIR 환경 변수 존재 유무 판단 코드 추가

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,14 @@ export class Config {
     constructor() {}
 
     readWatchDir() {
+        let configPath = path.join(__dirname, "config", "../config.json");
+        if (!fs.existsSync(configPath)) {
+            console.warn("config.json 파일이 존재하지 않습니다");
+            configPath = <string>process.env.WATCH_DIR;
+        }
+        if (!fs.existsSync(configPath)) {
+            throw new Error("WATCH_DIR 환경 변수가 존재하지 않습니다");
+        }
         this.option = JSON.parse(
             fs.readFileSync(
                 path.join(__dirname, "config", "../config.json"),
